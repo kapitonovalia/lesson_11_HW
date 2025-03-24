308f38d140b39e99aef9f6b1c334a3e037954d7c
kapitonovalia <aliiakapitonova@gmail.com>
2025-03-24 03:09:31 +0300

    fix prettier

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -10,21 +10,22 @@ test.describe('Login tests', async () => {
 
     console.log(await response.text())
     expect(response.status()).toBe(StatusCodes.OK)
-    expect(/^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(await response.text())).toBeTruthy()
+    expect(
+      /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/.test(await response.text()),
+    ).toBeTruthy()
   })
 
   test('Unsuccessful authorization delete method', async ({ request }) => {
     const response = await request.delete(`https://backend.tallinn-learning.ee/login/student`, {
-      data: LoginDTO.createLoginWithCorrectData()
+      data: LoginDTO.createLoginWithCorrectData(),
     })
     console.log(await response.text())
     expect(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
-
   })
 
   test('Unsuccessful authorization put method', async ({ request }) => {
     const response = await request.put(`https://backend.tallinn-learning.ee/login/student`, {
-      data: LoginDTO.createLoginWithCorrectData()
+      data: LoginDTO.createLoginWithCorrectData(),
     })
 
     console.log(await response.text())
@@ -32,11 +33,9 @@ test.describe('Login tests', async () => {
   })
   test('Incorrect credentials', async ({ request }) => {
     const response = await request.post('https://backend.tallinn-learning.ee/login/student', {
-      data: LoginDTO.createLoginWithIncorrectData()
-
+      data: LoginDTO.createLoginWithIncorrectData(),
     })
     console.log(await response.text())
     expect(response.status()).toBe(StatusCodes.UNAUTHORIZED)
   })
 })
-
